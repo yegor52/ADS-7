2f10b7740313c6adb7f5c274a29095cd26d44f9e
yegor52 <yegorbeat2005@gmail.com>
2024-04-27 13:16:58 +0300

    ADS 7

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,14 +2,61 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+#include <cstdint>
+
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
-
+ private:
+    struct VAL {
+        T data;
+        VAL *next;
+    };
+    TPQueue::VAL *create(const T &);
+    VAL *head;
+    VAL *tail;
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    T prior;
 };
+ public:
+    TPQueue() : head(nullptr), tail(nullptr) {}
+    ~TPQueue();
+    void push(const T &);
+    T pop();
+    void rmTail();
+ };
+template<typename T>
+void TPQueue<T>::push(const T &data) {
+    if (head == nullptr) {
+        tail = create(data);
+        head = tail;
+    } else if (head == tail) {
+        if (tail->data.prior < data.prior) {
+            tail->next = create(data);
+            tail = tail->next;
+        } else {
+            VAL *tempo = head;
+            head = create(data);
+            head->next = tempo;
+        }
+    } else {
+        VAL *temp = head;
+        if (data.prior > tail->data.prior) {
+            tail->next = create(data);
+            tail = tail->next;
+        } else if (data.prior < head->data.prior) {
+            VAL *t = head;
+            head = create(data);
+            head->next = t;
+        } else {
+            while (data.prior > temp->next->data.prior) {
+                temp = temp->next;
+            }
+            VAL *tem = temp->next;
+            temp->next = create(data);
+            temp->next->next = tem;
+        }
+    }
+}
 
 #endif  // INCLUDE_TPQUEUE_H_
